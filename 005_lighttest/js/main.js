1855fb342934f42d8149bd623718db1780f6e0dc
bazbarrett <bazerk@gmail.com>
2013-11-11 08:33:47 +0000

    Lighting test working pretty well

--- 005_lighttest/js/main.js
+++ 005_lighttest/js/main.js
@@ -1,4 +1,5 @@
 (function() {
+
   var worldTileData = {
     framerate: 1,
     images: ["/img/oryx/oryx_16bit_fantasy_world_trimmed.png"],
@@ -14,12 +15,38 @@
     }
   };
 
+  var creatureTileData = {
+    framerate: 1,
+    images: ["/img/oryx/oryx_16bit_fantasy_creatures_trans_trimmed.png"],
+    frames: {width: 24, height: 24, regX: 12, regY: 12},
+    animations: {
+      blue_warrior: { frames: [0, 18] },
+      black_mage: { frames: [3, 21] },
+    }
+  };
+
   var diagonals = [
     [1, 1],
     [1, -1],
     [-1, -1],
     [-1, 1]
   ];
+
+  function Player(tiles, frame, row, col) {
+    this.tiles = tiles;
+    this.frame = frame;
+    this.row = row;
+    this.col = col;
+    this.dir = 1;
+    this.tile = new createjs.Sprite(this.tiles, this.frame);
+    stage.addChild(this.tile);
+  }
+
+  Player.prototype.draw = function() {
+    this.tile.x = this.row*24 + 12;
+    this.tile.y = this.col*24 + 12;
+    this.tile.scaleX = this.dir;
+  };
 
   function MapSquare(tiles, brick, row, col) {
     this.brick = brick;
@@ -29,27 +56,32 @@
     this.col = col;
     this.stageX = row*24 + 12;
     this.stageY = col*24 + 12;
+    this.tile = new createjs.Sprite(this.tiles, this.frame);
+    this.tile.x = this.stageX;
+    this.tile.y = this.stageY;
+    this.shadow = new createjs.Shape();
+    this.shadow.graphics
+      .beginFill("rgb(0, 0, 0)")
+      .drawRect(this.stageX-12, this.stageY-12, 24, 24);
+    stage.addChild(this.tile);
+    stage.addChild(this.shadow);
   }
 
   MapSquare.prototype.draw = function(lightIntensity) {
-    var tile = new createjs.Sprite(this.tiles, this.frame);
-    tile.x = this.stageX;
-    tile.y = this.stageY;
-    var shadow = new createjs.Shape();
-    shadow.graphics
-      .beginFill("rgba(0, 0, 0, " + lightIntensity + ')')
-      .drawRect(this.stageX-12, this.stageY-12, 24, 24);
-    stage.addChild(tile);
-    stage.addChild(shadow);
+    this.shadow.alpha = lightIntensity;
   };
 
   function Map() {
     this.width = 16;
     this.height = 16;
-    this.stage = stage;
     this.rows = [];
     this.light = [];
     this.tiles = new createjs.SpriteSheet(worldTileData);
+    this.creatureTiles = new createjs.SpriteSheet(creatureTileData);
+    this.LEFT = 0;
+    this.RIGHT = 1;
+    this.UP = 2;
+    this.DOWN = 3;
   }
 
   Map.prototype.initialise = function() {
@@ -63,6 +95,8 @@
         this.rows[x].push(square);
       }
     }
+    this.player = new Player(this.creatureTiles, 'black_mage', 8, 8);
+    this.setFov();
   };
 
   Map.prototype.setLight = function(row, col, lightIntensity) {
@@ -75,9 +109,11 @@
       for (var col = 0; col < this.height; col++) {
         var lightRow = this.light[row];
         var light = lightRow ? lightRow[col] : 0;
+        if (typeof(light) === "undefined") light = 1;
         this.rows[row][col].draw(light);
       }
     }
+    this.player.draw();
   };
 
   Map.prototype.getSquare = function(x, y) {
@@ -86,13 +122,38 @@
     return this.rows[row][col];
   };
 
-  Map.prototype.setFov = function (square) {
+  Map.prototype.movePlayer = function(dir) {
+    var moveX = 0;
+    var moveY = 0;
+    switch (dir) {
+      case this.UP: moveY = -1; break;
+      case this.DOWN: moveY = 1; break;
+      case this.LEFT:
+        moveX = -1;
+        this.player.dir = 1;
+        break;
+      case this.RIGHT:
+        moveX = 1;
+        this.player.dir = -1;
+        break;
+    }
+    var newRow = this.player.row + moveX;
+    if ((newRow >= 0) && (newRow < this.width)) this.player.row = newRow;
+    var newCol = this.player.col + moveY;
+    if ((newCol >= 0) && (newCol < this.height)) this.player.col = newCol;
+    this.setFov();
+    this.draw();
+  };
+
+  Map.prototype.setFov = function() {
     map.light = [];
-    map.setLight(square.row, square.col, 0);
+    var row = map.player.row;
+    var col = map.player.col;
+    map.setLight(row, col, 0);
     for (var ixDiag = 0; ixDiag < diagonals.length; ixDiag++) {
       var d = diagonals[ixDiag];
-      this.castLight(square.row, square.col, 1, 1, 0, 0, d[0], d[1], 0);
-      this.castLight(square.row, square.col, 1, 1, 0, d[0], 0, 0, d[1]);
+      this.castLight(row, col, 1, 1, 0, 0, d[0], d[1], 0);
+      this.castLight(row, col, 1, 1, 0, d[0], 0, 0, d[1]);
     }
   };
 
@@ -138,17 +199,25 @@
   };
 
   var stage = new createjs.Stage(canvas);
-  stage.addEventListener('click', moveFovToSquare);
-
-  function moveFovToSquare(ev) {
-    var square = map.getSquare(ev.stageX, ev.stageY);
-    map.setFov(square);
-    map.draw();
-  }
-
-  var map = new Map(stage);
+  var map = new Map();
   map.initialise();
   map.draw();
+
+  kd.UP.press(function () {
+    map.movePlayer(map.UP);
+  });
+
+  kd.LEFT.press(function () {
+    map.movePlayer(map.LEFT);
+  });
+
+  kd.RIGHT.press(function () {
+    map.movePlayer(map.RIGHT);
+  });
+
+  kd.DOWN.press(function () {
+    map.movePlayer(map.DOWN);
+  });
 
   createjs.Ticker.addEventListener("tick", handleTick);
   function handleTick(ev) {
